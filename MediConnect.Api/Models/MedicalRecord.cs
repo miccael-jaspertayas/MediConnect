0eef75488b0f00ba07f71de7acbda392deac41e4
aeroo0 <165895786+aeroo0@users.noreply.github.com>
2026-07-12 14:28:28 +0800

    Update MedicalRecord.cs

--- MediConnect.Api/Models/MedicalRecord.cs
+++ MediConnect.Api/Models/MedicalRecord.cs
@@ -2,5 +2,14 @@
 {
     public class MedicalRecord
     {
+        public int RecordID { get; set; }
+        public int PatientID { get; set; }
+
+        public DateTime VisitDate { get; set; }
+
+        public string HospitalName { get; set; } = string.Empty;
+        public string DoctorName { get; set; } = string.Empty;
+        public string Diagnosis { get; set; } = string.Empty;
+        public string Notes { get; set; } = string.Empty;
     }
 }
