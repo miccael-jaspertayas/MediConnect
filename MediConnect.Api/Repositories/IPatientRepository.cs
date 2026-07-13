82854b1be7fb02a821963282813cbfac7c2c6657
Miccael Jasper Tayas <tayasmiccaeljasper@gmail.com>
2026-07-13 08:33:23 +0800

    Added PatientDto and IPatientRepository

---
+++ MediConnect.Api/Repositories/IPatientRepository.cs
@@ -0,0 +1,10 @@
+﻿using MediConnect.Api.Models;
+
+namespace MediConnect.Api.Repositories
+{
+    public interface IPatientRepository
+    {
+        Task<Patient?> GetByIdAsync(int patientId);
+        Task UpdateAsync(Patient patient);
+    }
+}
