d0ba384cdd8e8b60abe82cf62f1d32da450c631f
Miccael Jasper Tayas <139042848+miccael-jaspertayas@users.noreply.github.com>
2026-07-15 02:38:36 +0800

    Add VitalsService and clean up MauiProgram
    Added VitalsService as a singleton service and removed redundant code for app creation.

--- MediConnect.Mobile/MauiProgram.cs
+++ MediConnect.Mobile/MauiProgram.cs
@@ -19,6 +19,7 @@ public static class MauiProgram
 
         builder.Services.AddSingleton<ApiService>();
         builder.Services.AddSingleton<AuthService>();
+        builder.Services.AddSingleton<VitalsService>();
         builder.Services.AddSingleton<RecordsService>();
 
 
@@ -32,29 +33,6 @@ public static class MauiProgram
         builder.Services.AddTransient<LoginPage>();
         builder.Services.AddTransient<RegisterPage>();
         builder.Services.AddTransient<RecordsPage>();
-
-
-        return builder.Build();
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
-
-            builder.Services.AddSingleton<SessionService>();
-            builder.Services.AddSingleton<ApiService>();
-            builder.Services.AddTransient<AuthService>();
-            builder.Services.AddSingleton<VitalsService>();
-
-            builder.Services.AddTransient<LoginViewModel>();
-            builder.Services.AddTransient<LoginPage>();
-            builder.Services.AddTransient<RegisterViewModel>();
-            builder.Services.AddTransient<RegisterPage>();
 
 #if DEBUG
             builder.Logging.AddDebug();
