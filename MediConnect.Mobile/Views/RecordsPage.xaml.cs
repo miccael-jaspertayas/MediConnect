7307c2a83510ae208c7434ad3439ea4583851673
aeroo0 <165895786+aeroo0@users.noreply.github.com>
2026-07-15 01:28:13 +0800

    Day 4: UI Improve Part 1

--- MediConnect.Mobile/Views/RecordsPage.xaml.cs
+++ MediConnect.Mobile/Views/RecordsPage.xaml.cs
@@ -1,21 +1,43 @@
+using MediConnect.Mobile.Models;
 using MediConnect.Mobile.ViewModels;
 
 namespace MediConnect.Mobile.Views;
 
 public partial class RecordsPage : ContentPage
 {
-    private readonly RecordsViewModel _viewModel;
-
-    public RecordsPage(RecordsViewModel viewModel)
+    public RecordsPage(RecordsViewModel vm)
     {
         InitializeComponent();
-        BindingContext = viewModel;
-        _viewModel = viewModel;
+        BindingContext = vm;
+    }
+
+    private void EditButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is RecordsViewModel vm &&
+            sender is Button button &&
+            button.CommandParameter is MedicalRecord record)
+        {
+            vm.EditRecordCommand.Execute(record);
+        }
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        if (BindingContext is RecordsViewModel vm &&
+            sender is Button button &&
+            button.CommandParameter is MedicalRecord record)
+        {
+            vm.DeleteRecordCommand.Execute(record);
+        }
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-        await _viewModel.LoadRecordsAsync();
+
+        if (BindingContext is RecordsViewModel vm)
+        {
+            await vm.LoadRecordsAsync();
+        }
     }
 }
