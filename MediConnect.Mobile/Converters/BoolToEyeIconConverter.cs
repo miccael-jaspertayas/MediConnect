bc7ede51c485cd1c85a621ddf5c4e7d060a1c7a7
Miccael Jasper Tayas <tayasmiccaeljasper@gmail.com>
2026-07-15 23:51:08 +0800

    Finalized Medication Lookup + UI Enhancements

---
+++ MediConnect.Mobile/Converters/BoolToEyeIconConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace MediConnect.Mobile.Converters
+{
+    // true (hidden) -> closed eye emoji, false (visible) -> open eye emoji
+    public class BoolToEyeIconConverter : IValueConverter
+    {
+        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            var isHidden = value is bool b && b;
+            return isHidden ? "👁‍🗨" : "👁";
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
